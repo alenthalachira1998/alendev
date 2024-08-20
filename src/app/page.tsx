166424571f2e39b02b406de60e1f76e0dbdc7104
alenthalachira <alenthalachira.official@gmail.com>
2024-08-20 19:32:37 -0400

    firstmmit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,44 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './styles/Home.module.scss'
+import SideNav from "@/components/layouts/sidenav";
 
 export default function Home() {
+  const text = "Sorry, This website under construction";
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <header className={styles.header}>
+        <nav>
+       <SideNav/>
+        </nav>
+      </header>
+
+      <div 
+        className={styles.digitalTextTyping}
+        style={{
+          '--character-count': text.length,
+          '--typing-duration': `${text.length * 0.15}s`
+        } as React.CSSProperties}
+      >
+        {text}
       </div>
+      
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className={styles.imageContainer}>
+  <Image
+    src="/images/banner.png"
+    alt="Software Development Setup"
+    className={styles.mainImage}
+    width={600}
+    height={400}
+    priority
+    quality={100}
+    unoptimized
+  />
+</div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <Image width={50} height={50}  src= "/images/arrow.svg" alt="arrow" className={styles.scrollIndicator}/>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
